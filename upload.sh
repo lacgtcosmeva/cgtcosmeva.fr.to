19226b3890295fdcd00bf0a3d23aa1e336bca1ab
mirsella <mirsella@protonmail.com>
2020-08-07 20:28:53 +0200

    upload.sh

--- upload.sh
+++ upload.sh
@@ -4,5 +4,6 @@ git pull
 git add -A
 git commit -m "upload.sh $(date)"
 git push origin master
-echo 'appuie sur une touche pour quitter'; read
+echo 'appuie sur une touche pour quitter'
+read
 " && notify-send "Sucess"
