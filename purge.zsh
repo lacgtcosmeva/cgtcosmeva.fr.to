f32ad63477e532e26f68fe816f5ebb33860d5f6b
mirsella <mirsella@protonmail.com>
2020-08-29 12:42:16 +0200

    removed script

--- purge.zsh
+++
@@ -1,8 +0,0 @@
-index=$(rg -o '^\.[\w|-]*' static/css/style.css)
-
-while read class; do 
-  class=$(echo $class | sed 's/\.//g')
-  if ! rg -q $class index.html; then
-    echo $class is ununsed
-  fi
-done <<< $index
