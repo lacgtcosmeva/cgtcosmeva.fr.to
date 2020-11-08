22a569efdb6730ca795300076e9b97e1a49748fc
mirsella <mirsella@protonmail.com>
2020-11-08 10:21:41 +0100

    better only file listing

--- static/js/script.js
+++ static/js/script.js
@@ -11,7 +11,7 @@ try {
         document.getElementById("file-loading").innerHTML = ""
         for (classname of [ "historique", "statuts", "comptes", "tracts", "brochures" ]) {
           for (let file = 0; file < data.tree.length; file++) {
-            if (RegExp("^documents/" + classname + "/").test(data.tree[file].path) && data.tree[file].size) {
+            if (RegExp("^documents/" + classname + "/").test(data.tree[file].path) && data.tree[file].type == "blob") {
               console.log("match : " + data.tree[file].path)
               let link_shown = data.tree[file].path
               let link_href = link_shown.replace(/ /g, "%20")
