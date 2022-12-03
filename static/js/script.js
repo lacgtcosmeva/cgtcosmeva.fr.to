ebb0056587066bf05b4db7f68204a5b551e631db
mirsella <mirsella@protonmail.com>
2022-12-03 13:15:00 +0100

    fix tracts year

--- static/js/script.js
+++ static/js/script.js
@@ -2,51 +2,57 @@ let api_url = 'https://api.github.com/repos/lacgtcosmeva/documents/git/trees/mai
 let file_url = 'https://github.com/lacgtcosmeva/documents/raw/main/'
 
 function error() {
-  document.getElementById("file-loading").innerHTML = '<a href="https://github.com/lacgtcosmeva/lacgtcosmeva.github.io/tree/main/documents"><br>Network Error. clicker ici pour voir les fichiers</a>'
-  alert("connection erreur");
+	document.getElementById("file-loading").innerHTML = '<a href="https://github.com/lacgtcosmeva/lacgtcosmeva.github.io/tree/main/documents"><br>Network Error. clicker ici pour voir les fichiers</a>'
 }
 
 let tractsYear = document.getElementById("tracts-year")
+let content = document.querySelector('.card > .tractslist.list-content')
 const currentYear = new Date().getFullYear()
+tractsYear.value = 2014
+while (tractsYear.value <= currentYear) {
+	content.innerHTML += `<ul class="list-content tracts tracts${tractsYear.value}"></ul>`
+	tractsYear.valueAsNumber++
+}
 tractsYear.max = currentYear
+tractsYear.value = currentYear
 function changetracts() {
-  if (tractsYear.value < 2013) {
-    tractsYear.value = 2015
-  } else if (tractsYear > currentYear + 1) {
-    tractsYear.value = 2022
-  }
-  document.querySelectorAll(".tracts").forEach(e => {
-    e.className += " hidden"
-  }) 
-  document.querySelector(".tracts"+tractsYear.value).classList.remove("hidden")
+	if (tractsYear.value < tractsYear.min) {
+		tractsYear.value = tractsYear.min
+	} else if (tractsYear.value > currentYear) {
+		tractsYear.value = currentYear
+	}
+	document.querySelectorAll(".tracts").forEach(e => {
+		e.className += " hidden"
+	}) 
+	document.querySelector(".tracts"+tractsYear.value).classList.remove("hidden")
 }
 
 try {
-  fetch(api_url).then(response =>  {
-    if(response.ok) {
-      response.json().then(data => {
-        for (classname of [ "historique", "statuts", "comptes", "tracts", "brochures" ]) {
-          for (let file = 0; file < data.tree.length; file++) {
-            if (RegExp("^documents/" + classname + "/").test(data.tree[file].path) && data.tree[file].type == "blob") {
-              let link_shown = data.tree[file].path
-              let link_href = link_shown.replace(/ /g, "%20")
-              link_shown = link_shown.replace("documents/" + classname + "/", "")
-              if (classname == "tracts") {
-                document.getElementsByClassName(classname+data.tree[file].path.split("/")[2])[0].innerHTML  += `<li><a target="_blank" href=${file_url}${link_href}>${link_shown}</a></li>`
-              } else {
-                document.getElementsByClassName(classname)[0].innerHTML  += `<li><a target="_blank" href=${file_url}${link_href}>${link_shown}</a></li>`
-              }
-            }
-          }
-        }
-        changetracts()
-        document.getElementById("file-loading").innerHTML = ""
-      });
-    } else {
-      error()
-    }
-  })
-  .catch(function(error) {
-    error()
-  });
+	fetch(api_url).then(response =>  {
+		if(response.ok) {
+			response.json().then(data => {
+				for (classname of [ "historique", "statuts", "comptes", "tracts", "brochures" ]) {
+					for (let file = 0; file < data.tree.length; file++) {
+						if (RegExp("^documents/" + classname + "/").test(data.tree[file].path) && data.tree[file].type == "blob") {
+							let link_shown = data.tree[file].path
+							let link_href = link_shown.replace(/ /g, "%20")
+							link_shown = link_shown.replace("documents/" + classname + "/", "")
+							if (classname == "tracts") {
+								document.getElementsByClassName(classname+data.tree[file].path.split("/")[2])[0].innerHTML  += `<li><a target="_blank" href=${file_url}${link_href}>${link_shown}</a></li>`
+							} else {
+								document.getElementsByClassName(classname)[0].innerHTML  += `<li><a target="_blank" href=${file_url}${link_href}>${link_shown}</a></li>`
+							}
+						}
+					}
+				}
+				changetracts()
+				document.getElementById("file-loading").innerHTML = ""
+			});
+		} else {
+			error()
+		}
+	})
+		.catch(function(error) {
+			error()
+		});
 } catch(e) { error() }
